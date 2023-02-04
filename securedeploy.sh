c97d6e3f1ea7b3aa18f7881b6b3d9918793118c5
Josh <j.ecochard2@gmail.com>
2023-02-03 19:07:26 -0500

    fixing broken distro option

--- securedeploy.sh
+++ securedeploy.sh
@@ -41,7 +41,7 @@ Clear
     linode-cli regions ls --text --delimiter "," | awk -F"," '{print "\033[32m"$1" \t " $2" \033[0m";}' | tail -n+2
     read region_id
     
-    echo -e "${BYellow}Disable SSH for Root?: ${NC} (linode/debian11 or linode/ubunu22.04"
+    echo -e "${BYellow}Which Distro do you want to use?: ${NC} (linode/debian11 or linode/ubuntu22.04"
     read image_id
     
     echo -e "${BYellow}What should we call it?: ${NC}"
@@ -73,8 +73,8 @@ JSON="{
 
 #Use the CLI to deploy a secured Linode
     linode-cli linodes create \
-        --type $plan \
         --image $image_id \
+        --type $plan \
         --authorized_keys "$(cat $HOME/.ssh/id_rsa.pub))" \
         --root_pass $root_pass \
         --region  $region_id  \
